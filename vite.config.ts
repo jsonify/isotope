d2c60388b5335e60497c736427301a6f767b265d
Jason Rueckert <phlacin@gmail.com>
2025-03-21 23:49:31 -0700

    Add detailed documentation and update path alias in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,16 @@
+/***********************************************
+ * FILE: vite.config.ts
+ * CREATED: 2025-03-21 23:48:08
+ *
+ * PURPOSE:
+ * This file contains the configuration for Vite, the build tool.
+ * It defines plugins, alias configurations, and other build options.
+ *
+ * CONFIG SECTIONS:
+ * - plugins: Defines Vite plugins used in the build process (e.g., react, visualizer).
+ * - resolve: Configures module resolution, including path aliases.
+ ************************************************/
+
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
@@ -18,7 +31,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/Users/jsonify/code/isotope/src',
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
